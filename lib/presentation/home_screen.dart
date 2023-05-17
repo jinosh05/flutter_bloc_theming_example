e10a3403d033a5960e0e24a41ebcbd1859d49ca8
jinosh05 <jinoshprabhuraj5@gmail.com>
2023-05-18 00:36:18 +0530

    Animated Transition effect added

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -15,37 +15,43 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     ThemeCubit themeCubit = BlocProvider.of<ThemeCubit>(context, listen: true);
     bool isDark = themeCubit.isDark;
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        actions: [
-          IconButton(
-              onPressed: () {
-                themeCubit.toggleTheme();
-              },
-              icon: Icon(
-                isDark ? Icons.light_mode : Icons.dark_mode,
-                color: isDark
-                    ? ThemeData.light().canvasColor
-                    : ThemeData.dark().canvasColor,
-              ))
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text(
-              isDark ? 'Hello,\nGood night 🥱' : 'Hello,\nGood Morning 🌅 ',
-              textAlign: TextAlign.center,
-              style: const TextStyle(height: 1.5, fontSize: 20),
-            ),
-            Image.asset(
-              isDark ? 'assets/night.png' : 'assets/namaste.png',
-              height: 250,
-            ),
-            const SizedBox()
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 250),
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  themeCubit.toggleTheme();
+                },
+                icon: Icon(
+                  isDark ? Icons.light_mode : Icons.dark_mode,
+                  color: isDark
+                      ? ThemeData.light().canvasColor
+                      : ThemeData.dark().canvasColor,
+                ))
           ],
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                isDark ? 'Hello,\nGood night 🥱' : 'Hello,\nGood Morning 🌅 ',
+                textAlign: TextAlign.center,
+                style: const TextStyle(height: 1.5, fontSize: 20),
+              ),
+              Image.asset(
+                isDark ? 'assets/night.png' : 'assets/namaste.png',
+                height: 250,
+              ),
+              const SizedBox()
+            ],
+          ),
         ),
       ),
     );
